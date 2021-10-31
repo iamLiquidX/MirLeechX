ddd5c0a9a522013c08009d5b46aa4b2c36383f09
Sunil Kumar <66400864+iamLiquidX@users.noreply.github.com>
2021-10-31 12:38:46 +0530

    Minor typo

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 tracker_list=`curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt | awk '$1' | tr '\n' ',' | cat`
 qbit_trackers_list=$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt | awk '$0' | tr '\n' ',')
 echo -e "\nmax-concurrent-downloads=7\nbt-tracker=$tracker_list" >> /usr/src/app/aria.conf
-echo -e "Bittorrent\add_trackers=$=$qbit_trackers_list" >> /usr/src/app/qBittorrent.conf
+echo -e "\nBittorrent\add_trackers=$=$qbit_trackers_list" >> /usr/src/app/qBittorrent.conf
 aria2c --conf-path=/usr/src/app/aria.conf
 python3 -m bot
